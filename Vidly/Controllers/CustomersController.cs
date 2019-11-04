1056225b7f56ba8dd4d5ee33c0c4dea48b836725
Mahmoud <Mahmoud@DESKTOP-SOA6P1B>
2019-11-04 21:57:05 +0200

    Eager Loading

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,7 +22,7 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customer = _context.Customers.ToList();
+            var customer = _context.Customers.Include(c=>c.MemberShipType).ToList();
             return View(customer);
         }
         public ActionResult Details(int Id)
