9aceb2c939768428ae380bf9e97b583abbfbcd4f
Mahmoud <Mahmoud@DESKTOP-SOA6P1B>
2019-11-16 19:36:06 +0200

    Data Transfer Objects

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int CustomerId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+       
+        public string CustomerName { get; set; }
+
+        
+        [Min18IfAMember]
+        public DateTime? BirthDate { get; set; }
+        public bool IsSubscribedToNewsLatter { get; set; }
+     
+
+        [Display(Name = "Membership Type")]
+        public int MemberShipTypeId { get; set; }
+    }
+}
